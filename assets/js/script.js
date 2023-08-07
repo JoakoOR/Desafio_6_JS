e079772ac7067de9b0575ef6b60efa7567fa82ed
Joaquin <j.ortegaraggio@gmail.com>
2023-08-06 22:05:35 -0400

    Desafio_6 Conversor de Monedas sin Gráfico

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+const montoPesos = document.getElementById('montoPesos');
+const monedaSeleccionada = document.getElementById('seleccionador');
+const resultado = document.getElementById('resultado');
+const boton = document.getElementById('boton');
+const urlAPI = "https://mindicador.cl/api/";
+
+let valorUSD = "";
+let valorEUR = "";
+let valorUF = "";
+
+async function apiMonedas() {
+    const info = await fetch(urlAPI);
+    const datosAPI = await info.json();
+    valorUSD = datosAPI.dolar["valor"];
+    valorEUR = datosAPI.euro["valor"];
+    valorUF = datosAPI.uf["valor"];
+}
+
+async function calculo() {
+    try {
+        await apiMonedas();
+        if (monedaSeleccionada.value == "dolar" && montoPesos.value > 0) {
+            resultado.innerHTML = (montoPesos.value / valorUSD).toFixed(2).replace(".", ",") + " USD";
+
+        }
+        else if (monedaSeleccionada.value == "euro" && montoPesos.value > 0) {
+            resultado.innerHTML = (montoPesos.value / valorEUR).toFixed(2).replace(".", ",") + " EUR";
+
+        }
+        else if (monedaSeleccionada.value == "uf" && montoPesos.value > 0) {
+            resultado.innerHTML = (montoPesos.value / valorUF).toFixed(2).replace(".", ",") + " UF";
+        }
+        else {
+            alert("Seleccione una moneda válida o verifique que el monto ingresado sea mayor a cero.");
+        }
+    }
+    catch {
+        alert("Lo sentimos, estamos teniendo problemas, intente de nuevo mas tarde.")
+        console.log("Hay un error con la API")
+    }
+}
+
+boton.addEventListener("click", calculo);
